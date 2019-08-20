739dec965cf3eb5703987ed6862fcb53d0ced381
Tara Tandel <tara.tandel@piksel.com>
2019-08-20 17:39:44 +0200

    Prepare the setup and tearDown for the first test

--- What'sThisTests/What_sThisTests.swift
+++ What'sThisTests/What_sThisTests.swift
@@ -11,24 +11,25 @@ import XCTest
 
 class What_sThisTests: XCTestCase {
 
+    var viewC: ViewController!
+    
+    private var topLevelUIUtilities: TopLevelUIUtilities<ViewController>!
+
     override func setUp() {
+        super.setUp()
+        viewC = ViewController()
+        
+        topLevelUIUtilities = TopLevelUIUtilities<ViewController>()
+        topLevelUIUtilities.setupTopLevelUI(withViewController: viewC)
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
+        viewC = nil
+        topLevelUIUtilities.tearDownTopLevelUI()
+        topLevelUIUtilities = nil
+        super.tearDown()
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
