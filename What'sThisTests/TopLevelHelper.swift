1dcd172bf7edddf5da2d75a64c128625605860c1
Tara Tandel <tara.tandel@piksel.com>
2019-08-20 17:39:20 +0200

    Add a helper for test

---
+++ What'sThisTests/TopLevelHelper.swift
@@ -0,0 +1,37 @@
+//
+//  TopLevelHelper.swift
+//  What'sThisTests
+//
+//  Created by Tara Tandel on 20/08/2019.
+//  Copyright © 2019 Tara Tandel. All rights reserved.
+//
+
+import XCTest
+import UIKit
+
+class TopLevelUIUtilities<T: UIViewController> {
+    
+    private var rootWindow: UIWindow!
+    
+    func setupTopLevelUI(withViewController viewController: T) {
+        rootWindow = UIWindow(frame: UIScreen.main.bounds)
+        rootWindow.isHidden = false
+        rootWindow.rootViewController = viewController
+        _ = viewController.view
+        viewController.viewWillAppear(false)
+        viewController.viewDidAppear(false)
+    }
+    
+    func tearDownTopLevelUI() {
+        guard let rootWindow = rootWindow,
+            let rootViewController = rootWindow.rootViewController as? T else {
+                XCTFail("tearDownTopLevelUI() was called without setupTopLevelUI() being called first")
+                return
+        }
+        rootViewController.viewWillDisappear(false)
+        rootViewController.viewDidDisappear(false)
+        rootWindow.rootViewController = nil
+        rootWindow.isHidden = true
+        self.rootWindow = nil
+    }
+}
