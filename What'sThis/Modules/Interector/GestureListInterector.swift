7ed2ebbc74f41006af2ccb62f924f92ed7b789f3
Tara Tandel <tara.tandel@piksel.com>
2019-08-20 15:59:39 +0200

    Setup Interector

--- What'sThis/Modules/Interector/GestureListInterector.swift
+++ What'sThis/Modules/Interector/GestureListInterector.swift
@@ -11,5 +11,22 @@ import Foundation
 class GestureListInterector: GestureListInputInterectorProtocl {
     weak var presenter: GestureListOutputPresenterProtocol?
     
+    func fetchGestureData() {
+        presenter?.fetchIsComplete(gustures: fetchData())
+    }
     
+    private func fetchData() -> ArrayOfGestures? {
+        if let path = Bundle.main.path(forResource: "gestures", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let parsedData = try JSONDecoder().decode(ArrayOfGestures.self, from: data)
+                return parsedData
+            } catch  {
+                print("error")
+                return nil
+            }
+        } else {
+            return nil
+        }
+    }
 }
